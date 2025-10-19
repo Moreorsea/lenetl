3cd4824e565a88bdb0f60e727e7c1b168019329d
daryaWork <daraselanina89009@gmail.com>
2025-10-19 20:44:39 +0300

    add gtm package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  modules: ['nuxt-gtag'],
   app: {
     baseUrl: '/lenetl/'
   },
+  runtimeConfig: {
+    public: {
+      gtagId: process.env.NUXT_PUBLIC_GTAG_ID || ""
+    }
+  }
 })
