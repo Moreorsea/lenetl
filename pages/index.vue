c50dba5a22258a8a3df2a34dbe6a3b1c22b76352
daryaWork <daraselanina89009@gmail.com>
2025-10-19 21:36:56 +0300

    add event

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,29 @@
 <template>
   <h1>lenetl</h1>
-  <button class="button">click here</button>
+  <button class="button" @click="handleClick">click here</button>
 </template>
+
+<script setup>
+const { gtag } = useGtag()
+
+const trackUniversalEvent = (eventName, params) => {
+  // Отправка в GA4 через gtag
+  gtag('event', eventName, params)
+
+  // Отправка в GTM (если нужно)
+  if (window.dataLayer) {
+    window.dataLayer.push({
+      event: eventName,
+      ...params
+    })
+  }
+}
+
+const handleClick = () => {
+  console.log('HANDLE CLICK')
+  trackUniversalEvent('button_click', {
+    button_name: 'cta',
+    section: 'header'
+  })
+}
+</script>
